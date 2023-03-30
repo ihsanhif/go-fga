9a1f4f3eb3d2a5e9f5144ba4c3c5d1212b0fde14
Hafizul Ihsan Fadli <hafizulihsanfadli@gmail.com>
2023-03-30 14:20:07 +0700

    Challange 4

--- main.go
+++ main.go
@@ -2,30 +2,37 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"os"
 )
 
+type Teman struct {
+	Absen        int
+	Nama         string
+	Alamat       string
+	Pekerjaan    string
+	AlasanGolang string
+}
+
+var temanList = []Teman{
+	{1, "John Doe", "Jl. Merdeka No. 1", "Software Engineer", "Karena Golang memiliki performa yang cepat dan efisien."},
+	{2, "Jane Doe", "Jl. Sudirman No. 2", "Data Scientist", "Karena Golang sangat mudah dipelajari dan memiliki sintaks yang sederhana."},
+	{3, "Bob Smith", "Jl. Thamrin No. 3", "DevOps Engineer", "Karena Golang memiliki dukungan untuk concurrency dan parallelism yang kuat."},
+	{4, "Alice Brown", "Jl. Gatot Subroto No. 4", "Front-end Developer", "Karena Golang dapat digunakan untuk membangun aplikasi web yang scalable dan performant."},
+	{5, "Charlie Green", "Jl. Diponegoro No. 5", "Back-end Developer", "Karena Golang memiliki built-in library yang lengkap dan mudah digunakan."},
+}
+
 func main() {
-	str := "selamat malam"
-	words := strings.Split(str, " ")
+	absen := os.Args[1]
 
-	for _, word := range words {
-		for _, char := range word {
-			fmt.Printf("%c\n", char)
+	for _, teman := range temanList {
+		if fmt.Sprint(teman.Absen) == absen {
+			fmt.Println("Nama:", teman.Nama)
+			fmt.Println("Alamat:", teman.Alamat)
+			fmt.Println("Pekerjaan:", teman.Pekerjaan)
+			fmt.Println("Alasan memilih kelas Golang:", teman.AlasanGolang)
+			return
 		}
 	}
 
-	wordCount := make(map[string]int)
-	for _, word := range words {
-		for _, char := range word {
-			charString := string(char)
-			if _, ok := wordCount[charString]; ok {
-				wordCount[charString]++
-			} else {
-				wordCount[charString] = 1
-			}
-		}
-	}
-
-	fmt.Println(wordCount)
+	fmt.Println("Teman dengan absen", absen, "tidak ditemukan")
 }
