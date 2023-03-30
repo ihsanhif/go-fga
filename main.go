3f0d3fd962a0ad6ec0e8015f542e72d9da19c4aa
Hafizul Ihsan Fadli <hafizulihsanfadli@gmail.com>
2023-03-30 13:53:45 +0700

    Challenges 2

--- main.go
+++ main.go
@@ -5,22 +5,33 @@ import (
 )
 
 func main() {
-	var i int = 21
-	var j bool = true
-	var k float64 = 123.456
-	ya := 'Я'
+	str := "CAИAPBO"
 
-	fmt.Printf("%v \n", i)
-	fmt.Printf("%T \n", i)
-	fmt.Printf("%% \n")
-	fmt.Printf("%t \n", j)
-	fmt.Printf("%t \n", j)
-	fmt.Printf("%#X \n", ya)
-	fmt.Printf("%d \n", i)
-	fmt.Printf("%o \n", i)
-	fmt.Printf("%x \n", i)
-	fmt.Printf("%U \n", ya)
-	fmt.Printf("%.3f \n", k)
-	fmt.Printf("%f \n", k)
-	fmt.Printf("%E \n", k)
+	// Perulangan for untuk nilai i dari 0 hingga 4
+	for i := 0; i < 5; i++ {
+		fmt.Println("Nilai i =", i)
+	}
+
+	// Perulangan for untuk nilai j dari 0 hingga 10
+	for j := 0; j <= 10; j++ {
+		// Kondisi if-else untuk mencetak karakter berdasarkan nilai j
+		if j == 0 {
+			fmt.Println("Nilai j =", j)
+		} else if j == 6 {
+			fmt.Println("character U+041E 'O' starts at byte position 12")
+			fmt.Println("Nilai j =", j)
+		} else if j == 1 || j == 2 || j == 3 || j == 4 {
+			fmt.Println("Nilai j =", j)
+		} else if j == 5 {
+			for i, char := range str {
+				// cek apakah karakter adalah huruf besar
+				if char >= 'A' && char <= 'Z' {
+					// mencetak karakter unicode dan posisi byte dari karakter
+					fmt.Printf("character %U '%c' starts at byte position %d\n", char, char, i)
+				}
+			}
+		} else {
+			fmt.Println("Nilai j =", j)
+		}
+	}
 }
